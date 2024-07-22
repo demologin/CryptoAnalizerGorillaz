dc93473290b3446b7c0e5789658ec3774f2857e6
akhmelev <akhmelev@gmail.com>
2024-07-22 20:57:27 +0300

    ADD I started project CryptoAnalyzer

--- src/main/java/com/javarush/petrov/AppRunner.java
+++ src/main/java/com/javarush/petrov/AppRunner.java
@@ -2,6 +2,6 @@ package com.javarush.petrov;
 
 public class AppRunner {
     public static void main(String[] args) {
-        System.out.printlnfdghhfg("My super app");
+        System.out.println("My super app");
     }
 }
