28f404cb3e92e4c69bb3e66e1b46246c741f6b02
hollybible <kartzew1976@mail.ru>
2024-07-24 21:03:56 +0300

    Init commit

--- src/main/java/com/javarush/kartsev/MainApp.java
+++ src/main/java/com/javarush/kartsev/MainApp.java
@@ -3,6 +3,7 @@ package com.javarush.kartsev;
 public class MainApp {
     public static void main(String[] args) {
         // Логика для выбора режима работы, вызов соответствующих методов
+        System.out.println("Hello my first project");
     }
 }
 
