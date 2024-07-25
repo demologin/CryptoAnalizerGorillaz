a6bcf7cbdcd02b4f51ddbf2817db463ae2d426bb
Meeeat <angroov@gmail.com>
2024-07-25 11:48:47 +0900

    ADD Commands and Exeptions (just repeat)

--- src/main/java/com/javarush/siberia/Application.java
+++ src/main/java/com/javarush/siberia/Application.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class Application {
 
-    private MainController mainController;
+    private final MainController mainController;
     public Application() {
         mainController = new MainController();
     }
