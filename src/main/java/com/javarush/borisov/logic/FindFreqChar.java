670937c293478a81b76ac454973893437a33c530
Sergey Borisov <sergant@nk.ru>
2024-07-29 01:03:59 +0700

    try to create decryptor without alphabet  and metod of encrypt вроде работает, статистику ток подогнать и код рефакторить

--- src/main/java/com/javarush/borisov/logic/FindFreqChar.java
+++ src/main/java/com/javarush/borisov/logic/FindFreqChar.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FindFreqChar {
     protected static char findFrequentChar(String line) {
@@ -22,9 +24,10 @@ public class FindFreqChar {
         char key = 0;
         for (Map.Entry entry : chars.entrySet()) {
             if (Integer.parseInt(entry.getValue().toString()) > maxValue) {
-                maxValue = Integer.parseInt(entry.getValue().toString());
-                key = entry.getKey().toString().charAt(0);
-
+                if(!StatDecode.newAlphabet.containsValue(entry.getKey())) {
+                    maxValue = Integer.parseInt(entry.getValue().toString());
+                    key = entry.getKey().toString().charAt(0);
+                }
             }
         }
         System.out.println("чар: |" + key + "| частота встречи " + maxValue);
@@ -32,22 +35,47 @@ public class FindFreqChar {
 
     }
 
-    public static char findFrequentDoubleChar(String line, char knownChar) {
+    public static char findFrequentDoubleChar(String line, String regex, int regexLength, int returnPos) {
         Map<String, Integer> doubleChar = new HashMap<>();
-        for (int i = 0; i < line.length()-1; i++) {
-            doubleChar.put(line.charAt(i) + "" + line.charAt(i+1),1);
 
-        }
-        for (int i = 0; i < line.length(); i++) {
-            if (doubleChar.containsKey(line.charAt(i) +""+ knownChar)) {
+        Pattern pattern = Pattern.compile(regex);
 
-                doubleChar.put(((line.charAt(i) +"" + knownChar)), doubleChar.get((line.charAt(i) +"" + knownChar)) + 1);
+        for (int i = 0; i < line.length() - regexLength ; i++) {
+            String tmp = line.substring(i, i + regexLength );
+            Matcher matcher = pattern.matcher(tmp);
+            if (matcher.find()) {
+                if(doubleChar.containsKey(tmp)) {
+                    doubleChar.put(tmp, Integer.parseInt(doubleChar.get(tmp).toString()) + 1);
+                }else{
+                    doubleChar.put(tmp, 1);
+                }
             }
+
+
         }
 
+        //Matcher matcher = pattern.matcher(input);
+
+//        for (Map.Entry entry : doubleChar.entrySet()) {
+//            if (matcher.find()) {
+//                if (entry.getKey().toString().equals(matcher.group(knownChar.length()-1) + knownChar)) {
+//                    doubleChar.put(entry.getKey().toString(), Integer.parseInt(entry.getValue().toString()) + 1);
+//                }
+//            }
+//        }
 
 
-
+//        for (int i = 0; i < line.length(); i++) {
+//            if (doubleChar.containsKey(line.charAt(i) + knownChar)) {
+//
+//                {
+//
+//                    doubleChar.put(((line.charAt(i) + knownChar)), doubleChar.get((line.charAt(i) + knownChar)) + 1);
+//                }
+//            }
+//        }
+//
+//
         int maxValue = Integer.MIN_VALUE; // Начальное значение
         String key = "";
         for (Map.Entry entry : doubleChar.entrySet()) {
@@ -55,17 +83,18 @@ public class FindFreqChar {
 
             if (Integer.parseInt(entry.getValue().toString()) > maxValue) {
                 maxValue = Integer.parseInt(entry.getValue().toString());
-                key = entry.getKey().toString();
+                key = entry.getKey().toString().substring(0,regexLength);
 
             }
-
-
+//
+//
         }
         System.out.println("пара = |" + key + "|" + " частота встречи = |" + maxValue + "|");
 
 
         //System.out.println(doubleChar);
-        return key.charAt(0);
-    }
+        return key.charAt(returnPos);
 
+
+    }
 }
--- src/main/java/com/javarush/borisov/logic/StatDecode.java
+++ src/main/java/com/javarush/borisov/logic/StatDecode.java
@@ -1,4 +1,6 @@
 package com.javarush.borisov.logic;
+// todo заменять известные символы при дешифровке
+import com.sun.jdi.Value;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -7,35 +9,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StatDecode extends Reader {
-    private static Map<Character, Character> newAlphabet = new HashMap<>();
+    public static Map<Character, Character> newAlphabet = new HashMap<>();
     //  private static final Path encryptedFile = Path.of("c:\\111\\111.txt");
     private static Path encryptedFile = Path.of("c:\\111\\encrypt.txt");
     private static Path decryptedFile = Path.of("c:\\111\\decrypt.txt");
     private static Path tempFile = Path.of("c:\\111\\temp.txt");
     private static String line = "";
     private static int count = 1000;
+    private static final Map<Character, Integer> allSimbols= new HashMap<>();
+    private static final String rus = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String eng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String num = "1234567890";
+    private static final String cym = "!@#$%^&*()_+-=*/.,<>[]{};:'\" `~";
+    private static final String alphabet1 = (rus + rus.toLowerCase() + eng + eng.toLowerCase() + num + cym);
+    public static final char[] ALPHABET = alphabet1.toCharArray();
 
-    //    public void runStatDecode(){
+
+
     public static void main(String[] args) {
-        Reader.changeSimbol(encryptedFile,  tempFile, " " , "^!?^"); // заменяем то что сейчас является пробелом
-        line = Reader.read(tempFile);
-        addToNewAlphabet(findFrequentChar(line), ' ');// вычисляем пробелы и добавляем в новый алфавит
-        decrypt(tempFile, decryptedFile);
-        Reader.changeSimbol(decryptedFile, tempFile, "  " , " ");
-        line = Reader.read(tempFile);
-        addToNewAlphabet(findFrequentDoubleChar(line, ' '), 'о');// вычислили 'о' ??? добавляем в новый алфавит
-        line = "";
-        decrypt(tempFile,decryptedFile);
-        line = Reader.read(decryptedFile);
-        addToNewAlphabet(findFrequentDoubleChar(line, 'о'), 'в');// вычислили 'в' ??? добавляем в новый алфавит
+        char unused = searchUnusedSymbol();
 
+        Reader.changeSimbol(encryptedFile,  tempFile, " " , String.valueOf(unused)); // заменяем то что сейчас является пробелом
+        line = Reader.read(tempFile);
+
+       decrypt(tempFile, decryptedFile,addToNewAlphabet(findFrequentChar(line), ' '));// вычисляем пробелы и добавляем в новый алфавит и расшифровываем символ
+       //Reader.changeSimbol(decryptedFile, tempFile, "  " , " ");
+
+       line = Reader.read(decryptedFile);
+       decrypt(decryptedFile,tempFile,addToNewAlphabet(findFrequentDoubleChar(line, "\\S\\s",2 ,0), ','));// вычислили ','  добавляем в новый алфавит
+
+//
+      decrypt(tempFile,decryptedFile,addToNewAlphabet(findFrequentChar(line), 'о'));
+        line = Reader.read(decryptedFile);
+      // вычислили 'о' добавляем в новый алфавит
+       //char a = findFrequentChar(line);
+        decrypt(decryptedFile,tempFile,addToNewAlphabet(findFrequentDoubleChar(line, "\\s\\S\\s",3 ,1), 'и'));// вычислили 'в' ??? добавляем в новый алфавит
         System.out.println(newAlphabet);
 
 
     }
 
-    private static void addToNewAlphabet(char a, char b) {
+    private static char addToNewAlphabet(char a, char b) {
         newAlphabet.put(a, b);
+        return b;
     }
 
     private static char[] alphabetToCharArray(Map<Character, Character> alphabet) {
@@ -48,7 +64,7 @@ public class StatDecode extends Reader {
         return al;
     }
 
-    private static void decrypt(Path pathToRead, Path pathToSave) {
+    private static void decrypt(Path pathToRead, Path pathToSave,char checkedChar) {
 
         try (BufferedReader reader = Files.newBufferedReader(pathToRead);
              BufferedWriter writer = Files.newBufferedWriter(pathToSave)) {
@@ -64,20 +80,48 @@ public class StatDecode extends Reader {
 
                     if (newAlphabet.containsKey(tmp[j])) {
                         tmp[j] = newAlphabet.get(tmp[j]);
+                    }else if(newAlphabet.containsValue(tmp[j])) {
+                        for (Map.Entry<Character, Character> entry : newAlphabet.entrySet()) {
+                            if (entry.getValue().equals(tmp[j])&&newAlphabet.containsKey(tmp[j]) && newAlphabet.get(tmp[j]).equals(checkedChar)) {
+                                tmp[j] = entry.getKey();
+                            }
+                        }
                     }
+                }
 
-                }
-                if (count != 1) {
-                    writer.write((String.valueOf(tmp)).replaceAll("  ", " ") + "\n");
-                } else {
-                    writer.write((String.valueOf(tmp)).replaceAll("  ", " ") + "\n");
-                }
+                    writer.write(String.valueOf(tmp) + "\n");
+
             }
             count = 0;
 
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
+    }
+    private static char searchUnusedSymbol(){
+        line = Reader.read(encryptedFile);
+        for (int i = 0; i < line.length(); i++) {
+            if(allSimbols.containsKey(line.charAt(i))){
+                allSimbols.put(line.charAt(i), allSimbols.get(line.charAt(i))+1);
+            }else {
+                allSimbols.put(line.charAt(i), 1);
+            }
+
+
+        }
+        line = Reader.read(encryptedFile);
+
+        int minValue = Integer.MAX_VALUE;
+        char key='L' ;
+        for(Map.Entry<Character, Integer> entry : allSimbols.entrySet()){
+            if (Integer.parseInt(entry.getValue().toString()) < minValue) {
+                minValue = Integer.parseInt(entry.getValue().toString());
+                key = entry.getKey();
+            }
+        }
+        System.out.println("самый редкий символ = " + "|"+ key +"|" + " частота встречи - "+ minValue );
+        line="";
+        return key;
     }
 
 
