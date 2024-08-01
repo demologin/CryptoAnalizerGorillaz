c34bc55dd9c7acdec7cee9c5e7781a06541f14fe
akhmelev <akhmelev@gmail.com>
2024-08-01 20:04:39 +0300

    ADD module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.javarush.khmelov {
+    requires javafx.controls; //javafx
+    requires info.picocli;    //picocli
+    requires java.desktop;
+    requires java.logging;    //swing
+    exports com.javarush.khmelov;
+}
