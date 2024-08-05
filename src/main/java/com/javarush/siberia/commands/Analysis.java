22146160767e09e87c124949442fbd196b45cff8
Meeeat <angroov@gmail.com>
2024-08-05 19:56:30 +0900

    FIX analysis. remove dictionary

--- src/main/java/com/javarush/siberia/commands/Analysis.java
+++ src/main/java/com/javarush/siberia/commands/Analysis.java
@@ -5,28 +5,15 @@ import com.javarush.siberia.constants.Constants;
 import com.javarush.siberia.constants.FrequencyConstants;
 import com.javarush.siberia.entity.Result;
 import com.javarush.siberia.entity.ResultCode;
-import com.javarush.siberia.utils.DictionaryLoader;
 import com.javarush.siberia.utils.FileReadWrite;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class Analysis implements Action {
 
     private final CaesarCipher cipher = new CaesarCipher();
-    private final Set<String> ruWords;
-    private final Set<String> enWords;
-
-    public Analysis() {
-        try {
-            ruWords = DictionaryLoader.loadDictionary(Constants.RU_DICTIONARY_PATH);
-            enWords = DictionaryLoader.loadDictionary(Constants.EN_DICTIONARY_PATH);
-        } catch (IOException e) {
-            throw new RuntimeException("Ошибка загрузки словаря. Возможно файл отсутствует или повреждён", e);
-        }
-    }
 
     @Override
     public Result execute(String[] parameters) {
@@ -42,8 +29,8 @@ public class Analysis implements Action {
 
         for (int shift = 0; shift < Constants.ALPHABET.length; shift++) {
             char[] shiftedText = cipher.analysisDecryptText(text, shift, Constants.ALPHABET);
-            if (isMeaningful(shiftedText, ruWords, FrequencyConstants.RUSSIAN_FREQUENCY) ||
-                    isMeaningful(shiftedText, enWords, FrequencyConstants.ENGLISH_FREQUENCY)) {
+            if (isMeaningful(shiftedText, FrequencyConstants.RUSSIAN_FREQUENCY) ||
+                    isMeaningful(shiftedText, FrequencyConstants.ENGLISH_FREQUENCY)) {
                 try {
                     FileReadWrite.writeFile(outputFilePath, shiftedText);
                     return new Result("Удалось расшифровать текст. Сдвиг: " + shift, ResultCode.OK);
@@ -55,7 +42,7 @@ public class Analysis implements Action {
         return new Result("Не удалось расшифровать текст", ResultCode.ERROR);
     }
 
-    private boolean isMeaningful(char[] text, Set<String> dictionary, Map<Character, Double> frequencyMap) {
+    private boolean isMeaningful(char[] text, Map<Character, Double> frequencyMap) {
         String textStr = new String(text).toLowerCase();
         Map<Character, Integer> textFrequency = new HashMap<>();
 
@@ -63,7 +50,6 @@ public class Analysis implements Action {
             textFrequency.put(c, textFrequency.getOrDefault(c, 0) + 1);
         }
 
-        // Рассчитываем частотный балл
         double score = 0.0;
         int totalChars = textStr.length();
         for (Map.Entry<Character, Double> entry : frequencyMap.entrySet()) {
